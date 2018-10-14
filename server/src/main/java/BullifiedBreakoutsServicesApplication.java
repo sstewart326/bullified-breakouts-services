586788159a1aec37eb3c360253ac92c53357393f
Shawn <sstewart32619@gmail.com>
2018-10-13 22:40:37 -0400

    Project skeleton

---
+++ server/src/main/java/BullifiedBreakoutsServicesApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullifiedBreakoutsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullifiedBreakoutsServicesApplication.class, args);
+	}
+}
