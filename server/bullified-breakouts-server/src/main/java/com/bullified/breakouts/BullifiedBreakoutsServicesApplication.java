5985f29f44eaba5d57961fdb1b6dbd48fba0a784
Shawn <sstewart32619@gmail.com>
2018-10-14 01:07:09 -0400

    added controller skeleton

--- server/bullified-breakouts-server/src/main/java/com/bullified/breakouts/BullifiedBreakoutsServicesApplication.java
+++ server/bullified-breakouts-server/src/main/java/com/bullified/breakouts/BullifiedBreakoutsServicesApplication.java
@@ -1,7 +1,11 @@
+package com.bullified.breakouts;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.bullified.breakouts")
 public class BullifiedBreakoutsServicesApplication {
 
 	public static void main(String[] args) {
--- server/bullified-breakouts-server/src/main/java/com/bullified/breakouts/rest/BullifiedBreakoutsController.java
+++ server/bullified-breakouts-server/src/main/java/com/bullified/breakouts/rest/BullifiedBreakoutsController.java
@@ -1,4 +1,22 @@
 package com.bullified.breakouts.rest;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("/rest/1.0")
 public class BullifiedBreakoutsController {
+
+    @GetMapping("/health-check")
+    @ResponseBody
+    public String healthCheck() {
+        return "\"status\":\"success\"";
+    }
+
+    @PostMapping("/upload-file")
+    @ResponseBody
+    public String uploadFile(@RequestParam("fileName")MultipartFile file) {
+        return "\"status\":\"success\"";
+    }
 }
